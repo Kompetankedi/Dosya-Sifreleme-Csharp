5ff0106131e7377b514c8137e565454b8757e8d2
Kompetankedi <berkbag34@gmail.com>
2024-11-05 17:18:23 +0300

    Add project files.

---
+++ sifreleme/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace sifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifre";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(87, 48);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(234, 27);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(327, 48);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(141, 27);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Dosya seç";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(179, 81);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(86, 33);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Şifrele";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(87, 81);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(86, 30);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Şifre Çöz";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 234);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Dosya Şifreleme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ sifreleme/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace sifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string filePath;
+        private string password;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+        }
+        private byte[] GenerateKey(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private byte[] EncryptData(byte[] data, string password)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = GenerateKey(password);
+                aes.GenerateIV();
+
+                using (var ms = new MemoryStream())
+                {
+                    ms.Write(aes.IV, 0, aes.IV.Length);
+
+                    using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(data, 0, data.Length);
+                    }
+
+                    return ms.ToArray();
+                }
+            }
+        }
+
+
+        private byte[] DecryptData(byte[] encryptedData, string password)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = GenerateKey(password);
+
+                using (var ms = new MemoryStream(encryptedData))
+                {
+                    byte[] iv = new byte[aes.IV.Length];
+                    ms.Read(iv, 0, iv.Length);
+                    aes.IV = iv;
+
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        using (var result = new MemoryStream())
+                        {
+                            cs.CopyTo(result);
+                            return result.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    textBox1.Text = filePath;
+                }
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            password = txtPassword.Text;
+            if (string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(filePath))
+            {
+                MessageBox.Show("Lütfen dosya seçin ve şifre girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                byte[] data = File.ReadAllBytes(filePath);
+                byte[] encryptedData = EncryptData(data, password);
+                File.WriteAllBytes(filePath, encryptedData);
+                MessageBox.Show("Dosya başarıyla şifrelendi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Şifreleme sırasında hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            password = txtPassword.Text;
+            if (string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(filePath))
+            {
+                MessageBox.Show("Lütfen dosya seçin ve şifre girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                byte[] encryptedData = File.ReadAllBytes(filePath);
+                byte[] decryptedData = DecryptData(encryptedData, password);
+                File.WriteAllBytes(filePath, decryptedData);
+                MessageBox.Show("Dosya başarıyla çözüldü.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (CryptographicException)
+            {
+                MessageBox.Show("Yanlış şifre.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Şifre çözme sırasında hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
